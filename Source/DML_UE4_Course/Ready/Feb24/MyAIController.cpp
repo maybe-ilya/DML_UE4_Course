226cbc704deeadfe92174e79ff792b170c8a8ef3
napolinkinpark3r <napolinkinpark3r@gmail.com>
2016-03-01 22:49:51 +0300

    some preproduction for lesson

--- Source/DML_UE4_Course/Ready/Feb24/MyAIController.cpp
+++ Source/DML_UE4_Course/Ready/Feb24/MyAIController.cpp
@@ -5,10 +5,33 @@
 
 AMyAIController::AMyAIController(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
-	BlackboardComp = ObjectInitializer.CreateDefaultSubobject<UBlackboardComponent>(this, "Blackboard Component");
+	Blackboard = BlackboardComp = ObjectInitializer.CreateDefaultSubobject<UBlackboardComponent>(this, "Blackboard Component");
 
 	BrainComponent = BehaviorTreeComp = ObjectInitializer.CreateDefaultSubobject<UBehaviorTreeComponent>(this, "Behavior Tree Component");
 	BrainComponent->bAutoActivate = false;
+
+	PerceptionComp = ObjectInitializer.CreateDefaultSubobject<UAIPerceptionComponent>(this, "Perception Component");
+	SetPerceptionComponent(*PerceptionComp);
+	// Configuration of senses. I've tried to create them just like
+	// ordinary UObject (with NewObject and CreateObject), but failed.
+	HearingSense = ObjectInitializer.CreateDefaultSubobject<UAISenseConfig_Hearing>(this, "Hearing Sense");
+	HearingSense->DetectionByAffiliation.bDetectEnemies = true;
+	HearingSense->DetectionByAffiliation.bDetectFriendlies = true;
+	HearingSense->DetectionByAffiliation.bDetectNeutrals = true;
+	HearingSense->HearingRange = 1000.f;
+	PerceptionComp->ConfigureSense(*HearingSense);
+
+	SightSense = ObjectInitializer.CreateDefaultSubobject<UAISenseConfig_Sight>(this, "Sight Sense");
+	SightSense->DetectionByAffiliation.bDetectEnemies = true;
+	SightSense->DetectionByAffiliation.bDetectFriendlies = true;
+	SightSense->DetectionByAffiliation.bDetectNeutrals = true;
+	SightSense->PeripheralVisionAngleDegrees = 65.f;
+	SightSense->SightRadius = 1000.f;
+	SightSense->LoseSightRadius = 1500.f;
+	PerceptionComp->ConfigureSense(*SightSense);
+
+	PerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this, &AMyAIController::OnActorPerceptionUpdated);
+	PerceptionComp->OnPerceptionUpdated.AddDynamic(this, &AMyAIController::OnPerceptionUpdated);
 }
 
 void AMyAIController::BeginPlay()
@@ -25,4 +48,14 @@ void AMyAIController::Possess(class APawn* Pawn)
 		BlackboardComp->InitializeBlackboard(*BehaviorTreeAsset->BlackboardAsset);
 		BehaviorTreeComp->StartTree(*BehaviorTreeAsset);
 	}
+}
+
+void AMyAIController::OnActorPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+
+}
+
+void AMyAIController::OnPerceptionUpdated(TArray<AActor*> UpdatedActors)
+{
+
 }
--- Source/DML_UE4_Course/Ready/Feb24/MyAIController.h
+++ Source/DML_UE4_Course/Ready/Feb24/MyAIController.h
@@ -3,9 +3,15 @@
 #pragma once
 
 #include "AIController.h"
+// Includes for Behavior Tree and Blackboard
 #include "BehaviorTree/BehaviorTree.h"
 #include "BehaviorTree/BehaviorTreeComponent.h"
 #include "BehaviorTree/BlackboardComponent.h"
+// Includes for Perception
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Hearing.h"
+#include "Perception/AISenseConfig_Sight.h"
+
 #include "MyAIController.generated.h"
 
 /**
@@ -30,4 +36,15 @@ protected:
 		UBehaviorTreeComponent* BehaviorTreeComp;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI Components")
 		UBlackboardComponent* BlackboardComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI Components")
+		UAIPerceptionComponent* PerceptionComp;
+	// Perception senses
+	UAISenseConfig_Hearing* HearingSense;
+	UAISenseConfig_Sight* SightSense;
+
+public:
+	UFUNCTION(Category = "Perception Testing")
+		void OnActorPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+	UFUNCTION(Category = "Perception Testing")
+		void OnPerceptionUpdated(TArray<AActor*> UpdatedActors);
 };
